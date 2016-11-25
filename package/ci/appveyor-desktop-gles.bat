00c62ac3eca421aaf90f69419f8ac71b254e898b
Vladimír Vondruš <mosra@centrum.cz>
2016-11-25 19:39:24 +0100

    package/ci: add desktop GLES builds on Linux and Windows.

---
+++ package/ci/appveyor-desktop-gles.bat
@@ -0,0 +1,56 @@
+call "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat" x64 || exit /b
+set PATH=%APPVEYOR_BUILD_FOLDER%\deps\bin;%PATH%
+
+rem Build Corrade
+git clone --depth 1 git://github.com/mosra/corrade.git || exit /b
+cd corrade || exit /b
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Debug ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DWITH_INTERCONNECT=OFF ^
+    -G Ninja || exit /b
+cmake --build . || exit /b
+cmake --build . --target install || exit /b
+cd .. && cd ..
+
+rem Build Magnum
+git clone --depth 1 git://github.com/mosra/magnum.git || exit /b
+cd magnum || exit /b
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Debug ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DTARGET_GLES=ON ^
+    -DTARGET_GLES2=%TARGET_GLES2% ^
+    -DTARGET_DESKTOP_GLES=ON ^
+    -DWITH_AUDIO=OFF ^
+    -DWITH_DEBUGTOOLS=OFF ^
+    -DWITH_MESHTOOLS=OFF ^
+    -DWITH_PRIMITIVES=OFF ^
+    -DWITH_SCENEGRAPH=OFF ^
+    -DWITH_SHADERS=OFF ^
+    -DWITH_SHAPES=OFF ^
+    -DWITH_TEXT=OFF ^
+    -DWITH_TEXTURETOOLS=OFF ^
+    -DWITH_WINDOWLESSWGLAPPLICATION=ON ^
+    -G Ninja || exit /b
+cmake --build . || exit /b
+cmake --build . --target install || exit /b
+cd .. && cd ..
+
+rem Build
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Debug ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DWITH_BULLET=OFF ^
+    -DWITH_OVR=ON ^
+    -DBUILD_TESTS=ON ^
+    -DBUILD_GL_TESTS=ON ^
+    -G Ninja || exit /b
+cmake --build . || exit /b
+cmake --build . --target install || exit /b
+
+rem Test
+ctest -V -E GLTest || exit /b
---
+++ package/ci/travis-desktop-gles.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -ev
+
+# Corrade
+git clone --depth 1 git://github.com/mosra/corrade.git
+cd corrade
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DCMAKE_INSTALL_RPATH=$HOME/deps/lib \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_INTERCONNECT=OFF
+make -j install
+cd ../..
+
+# Magnum
+git clone --depth 1 git://github.com/mosra/magnum.git
+cd magnum
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DCMAKE_INSTALL_RPATH=$HOME/deps/lib \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DTARGET_GLES=ON \
+    -DTARGET_GLES2=$TARGET_GLES2 \
+    -DTARGET_DESKTOP_GLES=ON \
+    -DWITH_AUDIO=OFF \
+    -DWITH_DEBUGTOOLS=OFF \
+    -DWITH_MESHTOOLS=OFF \
+    -DWITH_PRIMITIVES=OFF \
+    -DWITH_SCENEGRAPH=ON \
+    -DWITH_SHADERS=OFF \
+    -DWITH_SHAPES=ON \
+    -DWITH_TEXT=OFF \
+    -DWITH_TEXTURETOOLS=OFF \
+    -DWITH_WINDOWLESS${PLATFORM_GL_API}APPLICATION=ON
+make -j install
+cd ../..
+
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_PREFIX_PATH="$HOME/deps" \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DCMAKE_INSTALL_RPATH=$HOME/deps/lib \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_BULLET=OFF \
+    -DWITH_OVR=OFF \
+    -DBUILD_TESTS=ON \
+    -DBUILD_GL_TESTS=ON
+make -j${JOBS_LIMIT}
+CORRADE_TEST_COLOR=ON ctest -V -E GLTest
