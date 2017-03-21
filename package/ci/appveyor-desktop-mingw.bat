c8715fb6c0e75ba77ab4e5458da55f5db71767cc
Vladimír Vondruš <mosra@centrum.cz>
2017-03-21 02:05:48 +0100

    package/ci: build BulletIntegration on Windows as well.

--- package/ci/appveyor-desktop-mingw.bat
+++ package/ci/appveyor-desktop-mingw.bat
@@ -1,6 +1,26 @@
 rem Workaround for CMake not wanting sh.exe on PATH for MinGW. AARGH.
 set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
-set PATH=C:\tools\mingw64\bin;%APPVEYOR_BUILD_FOLDER%\deps\bin;%PATH%
+set PATH=C:\tools\mingw64\bin;%APPVEYOR_BUILD_FOLDER%\deps\bin;%APPVEYOR_BUILD_FOLDER%\bullet\bin;%PATH%
+
+rem Build Bullet
+IF NOT EXIST %APPVEYOR_BUILD_FOLDER%\2.86.1.zip appveyor DownloadFile https://github.com/bulletphysics/bullet3/archive/2.86.1.zip || exit /b
+7z x 2.86.1.zip || exit /b
+cd bullet3-2.86.1 || exit /b
+mkdir build && cd build || exit /b
+rem CMake's Find module is not able to find Debug versions of the libraries
+cmake .. ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/bullet ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DUSE_GRAPHICAL_BENCHMARK=OFF ^
+    -DBUILD_CPU_DEMOS=OFF ^
+    -DBUILD_BULLET2_DEMOS=OFF ^
+    -DBUILD_OPENGL3_DEMOS=OFF ^
+    -DINSTALL_LIBS=ON ^
+    -DBUILD_UNIT_TESTS=OFF ^
+    -G "MinGW Makefiles" || exit /b
+cmake --build . --target install || exit /b
+cd .. && cd ..
 
 rem Build Corrade. Could not get Ninja to work, meh.
 git clone --depth 1 git://github.com/mosra/corrade.git || exit /b
@@ -27,7 +47,7 @@ cmake .. ^
     -DWITH_MESHTOOLS=OFF ^
     -DWITH_PRIMITIVES=OFF ^
     -DWITH_SCENEGRAPH=ON ^
-    -DWITH_SHADERS=OFF ^
+    -DWITH_SHADERS=ON ^
     -DWITH_SHAPES=ON ^
     -DWITH_TEXT=OFF ^
     -DWITH_TEXTURETOOLS=OFF ^
@@ -46,7 +66,7 @@ mkdir build && cd build || exit /b
 cmake .. ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
-    -DWITH_BULLET=OFF ^
+    -DWITH_BULLET=ON ^
     -DWITH_OVR=OFF ^
     -DBUILD_TESTS=ON ^
     -DBUILD_GL_TESTS=ON ^
--- package/ci/appveyor-desktop.bat
+++ package/ci/appveyor-desktop.bat
@@ -1,6 +1,25 @@
 call "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat" x64 || exit /b
 set PATH=%APPVEYOR_BUILD_FOLDER%\deps\bin;%PATH%
 
+rem Build Bullet
+IF NOT EXIST %APPVEYOR_BUILD_FOLDER%\2.86.1.zip appveyor DownloadFile https://github.com/bulletphysics/bullet3/archive/2.86.1.zip || exit /b
+7z x 2.86.1.zip || exit /b
+cd bullet3-2.86.1 || exit /b
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/bullet ^
+    -DCMAKE_BUILD_TYPE=Debug ^
+    -DUSE_GRAPHICAL_BENCHMARK=OFF ^
+    -DBUILD_CPU_DEMOS=OFF ^
+    -DBUILD_BULLET2_DEMOS=OFF ^
+    -DBUILD_OPENGL3_DEMOS=OFF ^
+    -DINSTALL_LIBS=ON ^
+    -DBUILD_UNIT_TESTS=OFF ^
+    -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON ^
+    -G Ninja || exit /b
+cmake --build . --target install || exit /b
+cd .. && cd ..
+
 rem Build Corrade
 git clone --depth 1 git://github.com/mosra/corrade.git || exit /b
 cd corrade || exit /b
@@ -25,9 +44,9 @@ cmake .. ^
     -DWITH_DEBUGTOOLS=OFF ^
     -DWITH_MESHTOOLS=OFF ^
     -DWITH_PRIMITIVES=OFF ^
-    -DWITH_SCENEGRAPH=OFF ^
-    -DWITH_SHADERS=OFF ^
-    -DWITH_SHAPES=OFF ^
+    -DWITH_SCENEGRAPH=ON ^
+    -DWITH_SHADERS=ON ^
+    -DWITH_SHAPES=ON ^
     -DWITH_TEXT=OFF ^
     -DWITH_TEXTURETOOLS=OFF ^
     -DWITH_WINDOWLESSWGLAPPLICATION=ON ^
@@ -36,12 +55,18 @@ cmake --build . || exit /b
 cmake --build . --target install || exit /b
 cd .. && cd ..
 
-rem Build
+rem Build. CMake is not able to find Debug Bullet libraries on their own so I
+rem have to force them in.
 mkdir build && cd build || exit /b
 cmake .. ^
     -DCMAKE_BUILD_TYPE=Debug ^
     -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
-    -DWITH_BULLET=OFF ^
+    -DCMAKE_PREFIX_PATH=%APPVEYOR_BUILD_FOLDER%/bullet ^
+    -DBULLET_COLLISION_LIBRARY=%APPVEYOR_BUILD_FOLDER%/bullet/lib/BulletCollision_Debug.lib ^
+    -DBULLET_DYNAMICS_LIBRARY=%APPVEYOR_BUILD_FOLDER%/bullet/lib/BulletDynamics_Debug.lib ^
+    -DBULLET_MATH_LIBRARY=%APPVEYOR_BUILD_FOLDER%/bullet/lib/LinearMath_Debug.lib ^
+    -DBULLET_SOFTBODY_LIBRARY=%APPVEYOR_BUILD_FOLDER%/bullet/lib/BulletSoftBody_Debug.lib ^
+    -DWITH_BULLET=ON ^
     -DWITH_OVR=ON ^
     -DBUILD_TESTS=ON ^
     -DBUILD_GL_TESTS=ON ^
