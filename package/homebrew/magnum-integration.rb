0304187a823a878977a9718077a06e4339379c7f
Vladimír Vondruš <mosra@centrum.cz>
2019-10-26 18:58:32 +0200

    package/homebrew: update for 2019.10.

--- package/homebrew/magnum-integration.rb
+++ package/homebrew/magnum-integration.rb
@@ -1,9 +1,9 @@
 class MagnumIntegration < Formula
   desc "Integration libraries for the Magnum C++11/C++14 graphics engine"
   homepage "https://magnum.graphics"
-  url "https://github.com/mosra/magnum-integration/archive/v2019.01.tar.gz"
-  # wget https://github.com/mosra/magnum-integration/archive/v2019.01.tar.gz -O - | sha256sum
-  sha256 "a1de0eeb4b9ff58a1d4320e5632a4c84f9e54bc554c22aba334cafa1e7c7e868"
+  url "https://github.com/mosra/magnum-integration/archive/v2019.10.tar.gz"
+  # wget https://github.com/mosra/magnum-integration/archive/v2019.10.tar.gz -O - | sha256sum
+  sha256 "c96a8237878ae7d74a8a4331122f96a50374d2eee6aed8f15bb039321d66fd48"
   head "git://github.com/mosra/magnum-integration.git"
 
   depends_on "cmake"
