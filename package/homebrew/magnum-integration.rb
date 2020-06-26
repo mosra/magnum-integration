308eb8086abbe4a8c56ca7db998054fb2d80e341
Vladimír Vondruš <mosra@centrum.cz>
2020-06-26 17:31:00 +0200

    package/homebrew: sync latest changes from the homebrew-magnum repo.

--- package/homebrew/magnum-integration.rb
+++ package/homebrew/magnum-integration.rb
@@ -6,17 +6,32 @@ class MagnumIntegration < Formula
   sha256 "c96a8237878ae7d74a8a4331122f96a50374d2eee6aed8f15bb039321d66fd48"
   head "git://github.com/mosra/magnum-integration.git"
 
-  depends_on "cmake"
-  depends_on "eigen"
-  depends_on "glm"
+  depends_on "cmake" => :build
+  depends_on "eigen" => :recommended
+  depends_on "glm" => :recommended
   depends_on "magnum"
-  depends_on "bullet"
-  depends_on "dartsim"
+  depends_on "bullet" => :recommended
+  depends_on "dartsim" => :optional
 
   def install
+    # Bundle ImGui
+    system "curl", "-L", "https://github.com/ocornut/imgui/archive/v1.76.tar.gz", "-o", "src/MagnumExternal/imgui.tar.gz"
+    cd "src/MagnumExternal" do
+      system "mkdir", "ImGui"
+      system "tar", "xzvf", "imgui.tar.gz", "-C", "ImGui", "--strip-components=1"
+    end
+
     system "mkdir build"
     cd "build" do
-      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DWITH_BULLET=ON", "-DWITH_DART=ON", "-DWITH_EIGEN=ON", "-DWITH_GLM=ON", "-DWITH_IMGUI=OFF", ".."
+      system "cmake",
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+        "-DWITH_BULLET=#{(build.with? 'bullet') ? 'ON' : 'OFF'}",
+        "-DWITH_DART=#{(build.with? 'dartsim') ? 'ON' : 'OFF'}",
+        "-DWITH_EIGEN=#{(build.with? 'eigen') ? 'ON' : 'OFF'}",
+        "-DWITH_GLM=#{(build.with? 'glm') ? 'ON' : 'OFF'}",
+        "-DWITH_IMGUI=ON",
+        ".."
       system "cmake", "--build", "."
       system "cmake", "--build", ".", "--target", "install"
     end
