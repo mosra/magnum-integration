ca587ab5563e78bfed910d55c5a47167640841a9
Vladimír Vondruš <mosra@centrum.cz>
2022-02-14 19:05:32 +0100

    package/ci: add an iOS build.

---
+++ package/ci/ios-simulator-gles.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+set -ev
+
+git submodule update --init
+
+# Corrade
+git clone --depth 1 https://github.com/mosra/corrade.git
+cd corrade
+
+# Build native corrade-rc
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps-native \
+    -DWITH_INTERCONNECT=OFF \
+    -DWITH_PLUGINMANAGER=OFF \
+    -DWITH_TESTSUITE=OFF \
+    -DWITH_UTILITY=OFF \
+    -G Ninja
+ninja install
+cd ..
+
+# Crosscompile Corrade
+mkdir build-ios && cd build-ios
+cmake .. \
+    -DCMAKE_TOOLCHAIN_FILE=../../toolchains/generic/iOS.cmake \
+    -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk \
+    -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DBUILD_STATIC=ON \
+    -DTESTSUITE_TARGET_XCTEST=ON \
+    -DWITH_INTERCONNECT=OFF \
+    -DBUILD_STATIC=ON \
+    -G Xcode
+set -o pipefail && cmake --build . --config Release --target install | xcbeautify
+cd ../..
+
+# Crosscompile SDL. On 2022-14-02 curl says the certificate is expired, so
+# ignore that.
+# TODO use a CMake build instead
+curl --insecure -O https://www.libsdl.org/release/SDL2-2.0.10.tar.gz
+tar -xzvf SDL2-2.0.10.tar.gz
+cd SDL2-2.0.10/Xcode-iOS/SDL
+set -o pipefail && xcodebuild -sdk iphonesimulator13.4 | xcbeautify
+cp build/Release-iphonesimulator/libSDL2.a $HOME/deps/lib
+mkdir -p $HOME/deps/include/SDL2
+cp -R ../../include/* $HOME/deps/include/SDL2
+cd ../../..
+
+# Crosscompile Magnum
+git clone --depth 1 https://github.com/mosra/magnum.git
+cd magnum
+mkdir build-ios && cd build-ios
+cmake .. \
+    -DCMAKE_TOOLCHAIN_FILE=../../toolchains/generic/iOS.cmake \
+    -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk \
+    -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DWITH_AUDIO=OFF \
+    -DWITH_DEBUGTOOLS=ON \
+    -DWITH_MESHTOOLS=ON \
+    -DWITH_PRIMITIVES=ON \
+    -DWITH_SCENEGRAPH=OFF \
+    -DWITH_SCENETOOLS=OFF \
+    -DWITH_SHADERS=ON \
+    -DWITH_SHADERTOOLS=OFF \
+    -DWITH_TEXT=OFF \
+    -DWITH_TEXTURETOOLS=OFF \
+    -DWITH_OPENGLTESTER=ON \
+    -DWITH_SDL2APPLICATION=ON \
+    -DTARGET_GLES2=$TARGET_GLES2 \
+    -DBUILD_STATIC=ON \
+    -G Xcode
+set -o pipefail && cmake --build . --config Release --target install | xcbeautify
+cd ../..
+
+# Crosscompile. There's extra crazy stuff for Eigen3. It's header-only but the
+# archive is so stupid that it's not possible to just use Eigen3Config.cmake,
+# as it's generated using CMake from Eigen3Config.cmake.in. There's
+# FindEigen3.cmake next to it, but that doesn't help with ANYTHING AT ALL
+# (like, what about looking one directory up, eh?! too hard?!) and also defines
+# just EIGEN3_INCLUDE_DIR, not the Eigen3::Eigen target nor
+# EIGEN3_INCLUDE_DIRS. Now I get why people hate CMake. It's because project
+# maintainers are absolutely clueless on how to write usable find scripts with
+# it.
+mkdir build-ios && cd build-ios
+cmake .. \
+    -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/iOS.cmake \
+    -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk \
+    -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+    -DCMAKE_MODULE_PATH=$HOME/eigen/cmake/ \
+    -DEIGEN3_INCLUDE_DIR=$HOME/eigen/ \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_PREFIX_PATH=$HOME/deps \
+    -DGLM_INCLUDE_DIR=$HOME/glm \
+    -DIMGUI_DIR=$HOME/imgui \
+    -DWITH_BULLET=OFF \
+    -DWITH_DART=OFF \
+    -DWITH_EIGEN=ON \
+    -DWITH_GLM=ON \
+    -DWITH_IMGUI=ON \
+    -DWITH_OVR=OFF \
+    -DBUILD_STATIC=ON \
+    -DBUILD_TESTS=ON \
+    -DBUILD_GL_TESTS=ON \
+    -G Xcode
+set -o pipefail && cmake --build . --config Release | xcbeautify
+# TODO: find a better way to avoid
+# Library not loaded: /System/Library/Frameworks/OpenGLES.framework/OpenGLES
+# error
+DYLD_FALLBACK_LIBRARY_PATH="/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS 12.4.simruntime/Contents/Resources/RuntimeRoot/System/Library/Frameworks/OpenGLES.framework" DYLD_FALLBACK_FRAMEWORK_PATH="/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS 12.4.simruntime/Contents/Resources/RuntimeRoot/System/Library/Frameworks" CORRADE_TEST_COLOR=ON ctest -V -C Release -E GLTest
+
+# Test install, after running the tests as for them it shouldn't be needed
+set -o pipefail && cmake --build . --config Release --target install | xcbeautify
