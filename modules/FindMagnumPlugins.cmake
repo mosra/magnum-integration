bf09698491f2061733dc263f375da1f02f41d8ec
Vladimír Vondruš <mosra@centrum.cz>
2024-03-19 13:41:03 +0100

    modules: updated FindMagnumPlugins.cmake.

--- modules/FindMagnumPlugins.cmake
+++ modules/FindMagnumPlugins.cmake
@@ -155,8 +155,14 @@ foreach(_component ${MagnumPlugins_FIND_COMPONENTS})
 endforeach()
 find_package(Magnum REQUIRED ${_MAGNUMPLUGINS_DEPENDENCIES})
 
-# Global plugin include dir
-find_path(MAGNUMPLUGINS_INCLUDE_DIR MagnumPlugins
+# Global include dir that's unique to Magnum Plugins. Often they will be
+# installed alongside Magnum, which is why the hint, but if not, it shouldn't
+# just pick MAGNUM_INCLUDE_DIR because then _MAGNUMPLUGINS_*_INCLUDE_DIR will
+# fail to be found. In case of CMake subprojects the versionPlugins.h is
+# generated inside the build dir so this won't find it, instead
+# src/CMakeLists.txt forcibly sets MAGNUMPLUGINS_INCLUDE_DIR as an internal
+# cache value to make that work.
+find_path(MAGNUMPLUGINS_INCLUDE_DIR Magnum/versionPlugins.h
     HINTS ${MAGNUM_INCLUDE_DIR})
 mark_as_advanced(MAGNUMPLUGINS_INCLUDE_DIR)
 
@@ -465,6 +471,16 @@ foreach(_component ${MagnumPlugins_FIND_COMPONENTS})
             find_package(OpenEXR REQUIRED MODULE)
             set_property(TARGET MagnumPlugins::${_component} APPEND PROPERTY
                 INTERFACE_LINK_LIBRARIES OpenEXR::OpenEXR)
+            # OpenEXR uses exceptions, which need an explicit flag on
+            # Emscripten. This is most likely not propagated through its CMake
+            # config file, so doing that explicitly here.
+            if(CORRADE_TARGET_EMSCRIPTEN)
+                if(CMAKE_VERSION VERSION_LESS 3.13)
+                    message(FATAL_ERROR "CMake 3.13+ is required in order to specify Emscripten linker options")
+                endif()
+                set_property(TARGET MagnumPlugins::${_component} APPEND PROPERTY
+                    INTERFACE_LINK_OPTIONS "SHELL:-s DISABLE_EXCEPTION_CATCHING=0")
+            endif()
 
         # No special setup for the OpenDdl library
         # OpenGexImporter has no dependencies
@@ -510,7 +526,7 @@ foreach(_component ${MagnumPlugins_FIND_COMPONENTS})
         elseif(_component STREQUAL StbImageImporter)
             # To solve a LTO-specific linker error. See StbImageImporter's
             # CMakeLists.txt for details.
-            if(CORRADE_TARGET_EMSCRIPTEN AND NOT EMSCRIPTEN_VERSION VERSION_LESS 3.1.42)
+            if(CORRADE_TARGET_EMSCRIPTEN AND NOT EMSCRIPTEN_VERSION VERSION_LESS 3.1.42 AND EMSCRIPTEN_VERSION VERSION_LESS 3.1.46)
                 if(CMAKE_VERSION VERSION_LESS 3.13)
                     message(FATAL_ERROR "CMake 3.13+ is required in order to specify Emscripten linker options")
                 endif()
@@ -524,7 +540,7 @@ foreach(_component ${MagnumPlugins_FIND_COMPONENTS})
         elseif(_component STREQUAL StbVorbisAudioImporter)
             # To solve a LTO-specific linker error. See StbVorbisAudioImporter's
             # CMakeLists.txt for details.
-            if(CORRADE_TARGET_EMSCRIPTEN AND NOT EMSCRIPTEN_VERSION VERSION_LESS 3.1.42)
+            if(CORRADE_TARGET_EMSCRIPTEN AND NOT EMSCRIPTEN_VERSION VERSION_LESS 3.1.42 AND EMSCRIPTEN_VERSION VERSION_LESS 3.1.46)
                 if(CMAKE_VERSION VERSION_LESS 3.13)
                     message(FATAL_ERROR "CMake 3.13+ is required in order to specify Emscripten linker options")
                 endif()
