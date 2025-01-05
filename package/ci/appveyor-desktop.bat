daf7127d4c48539b1008b0a639b97cdc8856b394
Vladimír Vondruš <mosra@centrum.cz>
2025-01-05 17:54:17 +0100

    package/ci: use grcov for code coverage on clang-cl and MinGW.
    And drop the lcov madness.

--- package/ci/appveyor-desktop.bat
+++ package/ci/appveyor-desktop.bat
@@ -4,11 +4,14 @@ if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" call "C:/Program File
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" call "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat" x64 || exit /b
 set PATH=%APPVEYOR_BUILD_FOLDER%\deps\bin;%PATH%
 
-rem need to explicitly specify a 64-bit target, otherwise CMake+Ninja can't
-rem figure that out -- https://gitlab.kitware.com/cmake/cmake/issues/16259
-rem for TestSuite we need to enable exceptions explicitly with /EH as these are
-rem currently disabled -- https://github.com/catchorg/Catch2/issues/1113
-if "%COMPILER%" == "msvc-clang" if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2022" set COMPILER_EXTRA=-DCMAKE_C_COMPILER="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/bin/clang-cl.exe" -DCMAKE_CXX_COMPILER="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/bin/clang-cl.exe" -DCMAKE_LINKER="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/bin/lld-link.exe" -DCMAKE_C_FLAGS="-m64 /EHsc" -DCMAKE_CXX_FLAGS="-m64 /EHsc"
+rem - Need to explicitly specify a 64-bit target, otherwise CMake+Ninja can't
+rem   figure that out -- https://gitlab.kitware.com/cmake/cmake/issues/16259
+rem - For TestSuite we need to enable exceptions explicitly with /EH as these
+rem   are currently disabled -- https://github.com/catchorg/Catch2/issues/1113
+rem - The MSVC 2022 build has coverage enabled, for which the profile library
+rem   has to be explicitly linked -- https://bugs.llvm.org/show_bug.cgi?id=40877,
+rem   https://gitlab.kitware.com/cmake/cmake/-/issues/23437
+if "%COMPILER%" == "msvc-clang" if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2022" set COMPILER_EXTRA=-DCMAKE_C_COMPILER="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/bin/clang-cl.exe" -DCMAKE_CXX_COMPILER="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/bin/clang-cl.exe" -DCMAKE_LINKER="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/bin/lld-link.exe" -DCMAKE_C_FLAGS="-m64 /EHsc --coverage" -DCMAKE_CXX_FLAGS="-m64 /EHsc --coverage" -DCMAKE_EXE_LINKER_FLAGS="-libpath:\"C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/lib/clang/18/lib/windows\" \"C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/lib/clang/18/lib/windows/clang_rt.profile-x86_64.lib\"" -DCMAKE_SHARED_LINKER_FLAGS="-libpath:\"C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/lib/clang/18/lib/windows\" \"C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/lib/clang/18/lib/windows/clang_rt.profile-x86_64.lib\""
 if "%COMPILER%" == "msvc-clang" if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2019" set COMPILER_EXTRA=-DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/Llvm/bin/clang-cl.exe" -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/Llvm/bin/clang-cl.exe" -DCMAKE_LINKER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/Llvm/bin/lld-link.exe" -DCMAKE_C_FLAGS="-m64 /EHsc" -DCMAKE_CXX_FLAGS="-m64 /EHsc"
 
 rem Build Bullet
@@ -119,3 +122,15 @@ ctest -V -E GLTest --timeout 60 || exit /b
 
 rem Test install, after running the tests as for them it shouldn't be needed
 cmake --build . --target install || exit /b
+
+cd %APPVEYOR_BUILD_FOLDER%
+
+rem Gather and upload coverage on the clang-cl MSVC 2022 build. Keep in sync
+rem with circleci.yml, appveyor-desktop-mingw.bat and PKBUILD-coverage, please.
+if "%COMPILER%" == "msvc-clang" if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2022" grcov build -t lcov --keep-only "*/src/Magnum/*" --ignore "*/Test/*" --ignore "*/build/src/*" -o coverage.info --excl-line LCOV_EXCL_LINE --excl-start LCOV_EXCL_START --excl-stop LCOV_EXCL_STOP || exit /b
+rem Official docs say "not needed for public repos", in reality not using the
+rem token is "extremely flakey". What's best is that if the upload fails, the
+rem damn thing exits with a success error code, and nobody cares:
+rem https://github.com/codecov/codecov-circleci-orb/issues/139
+rem https://community.codecov.com/t/commit-sha-does-not-match-circle-build/4266
+if "%COMPILER%" == "msvc-clang" if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2022" codecov -f ./coverage.info -t c2ea9495-5454-4294-a51d-4cadf575b325 -X gcov || exit /b
