4f65ad340cd297754e1d933301946289a52b74e6
Vladimír Vondruš <mosra@centrum.cz>
2025-11-08 18:37:32 +0100

    package: sync also the FindExtras module for YogaIntegration.

--- package/sync-modules.sh
+++ package/sync-modules.sh
@@ -6,3 +6,4 @@ dir=$(dirname "$0")
 cp $dir/../../corrade/modules/FindCorrade.cmake $dir/../modules/
 cp $dir/../../magnum/modules/FindMagnum.cmake $dir/../modules/
 cp $dir/../../magnum-plugins/modules/FindMagnumPlugins.cmake $dir/../modules/
+cp $dir/../../magnum-extras/modules/FindMagnumExtras.cmake $dir/../modules/
